54293f5220b9e07a036fa7774e1a4195f8fa5759
Slaaashi <oscar.johansson.ee14e@gmail.com>
2017-03-23 13:20:09 +0100

    Maps

--- scripts.js
+++ scripts.js
@@ -19,3 +19,15 @@ function showDivs(n) {
     }
     x[slideIndex-1].style.display = "block"; 
 }
+//Google Maps
+function initMap() {
+	var uluru = {lat: -25.363, lng: 131.044};
+    var map = new google.maps.Map(document.getElementById('map'), {
+		zoom: 4,
+        center: uluru
+        });
+	var marker = new google.maps.Marker({
+		position: uluru,
+        map: map
+        });
+      }
